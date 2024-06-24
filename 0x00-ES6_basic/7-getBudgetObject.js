8b2fc7560e8360006b7d1d305d3c914051a4de04
jxkimathi <judescriva8@gmail.com>
2024-06-25 00:20:43 +0300

    corrections

---
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -0,0 +1,9 @@
+export default function getBudgetObject(income, gdp, capita) {
+  const budget = {
+    income: this.income,
+    gdp: this.gdp,
+    capita: this.capita,
+  };
+  
+  return budget;
+}

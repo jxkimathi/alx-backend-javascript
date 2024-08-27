a366685d7ca51ed039d6798dc22c16b3687e6abb
jxkimathi <judescriva8@gmail.com>
2024-08-27 09:31:10 +0300

    correction on the test cases

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -1,14 +1,17 @@
-function calculateNumber (a, b, type) {
+const calculateNumber = (a, b, type) => {
   if (type === 'SUM') {
     return Math.round(a) + Math.round(b);
-  } else if (type === 'SUBTRACT') {
-    return Math.round(a) - Math.round(b);
-  } else if (type === 'DIVIDE') {
+  }
+  if (type === 'SUBTRACT') {
+    return Math.round(b) - Math.round(a);
+  }
+  if (type === 'DIVIDE') {
     if (Math.round(b) === 0) {
       return 'Error';
     }
     return Math.round(a) / Math.round(b);
   }
+  return 0;
 }
 
 module.exports = calculateNumber;
--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -1,17 +1,58 @@
 const assert = require('assert');
 const calculateNumber = require('./1-calcul');
 
-describe('calculateNumber', function() {
-  it('should return 4 when type is SUM and inputs are 1 and 3', function() {
-    assert.strictEqual(calculateNumber(1, 3, 'SUM'), 4);
+describe('calculateNumber', () => {
+  describe('type == "SUM"', () => {
+    it('should return 4 when type is SUM and inputs are 1 and 3', () => {
+      assert.strictEqual(calculateNumber(1, 3, 'SUM'), 4);
+    });
+
+    it('should return 5 when type is SUM and inputs are 1.2 and 3.8', () => {
+      assert.strictEqual(calculateNumber(1.2, 3.8, 'SUM'), 5);
+    });
+
+    it('should return 5 when type is SUM and inputs are 1.5 and 3.5', () => { 
+      assert.strictEqual(calculateNumber(1.5, 3.5, 'SUM'), 5);
+    });
+
+    it('should return 6 when type is SUM and inputs are 1.5 and 3.49', () => {
+      assert.strictEqual(calculateNumber(1.5, 3.49, 'SUM'), 6);
+    });
   });
-  it('should return 2 when type is SUBTRACT and inputs are 3 from 5', function() {
-    assert.strictEqual(calculateNumber(5, 3, 'SUBTRACT'), 2);
+
+  describe('type == ""SUBTRACT""', () => {
+    it('should return 2 when type is SUBTRACT and inputs are 3 and 5', () => {
+      assert.strictEqual(calculateNumber(3, 5, 'SUBTRACT'), 2);
+    });
+
+    it('should return -2 when type is SUBTRACT and inputs are 3.5 and 1.5', () => {
+      assert.strictEqual(calculateNumber(3.5, 1.5, 'SUBTRACT'), 2);
+    });
+
+    it('should return -1 when type is SUBTRACT and inputs are 3.5 and 4.49', () => {
+      assert.strictEqual(calculateNumber(3.5, 4.49, 'SUBTRACT'), -1);
+    });
+
+    it('should return -1 when type is SUBTRACT and inputs are 3.5 and 4.5', () => {
+      assert.strictEqual(calculateNumber(3.5, 4.5, 'SUBTRACT'), -1);
+    });
   });
-  it('should return 1 when type is DIVIDE and inputs are 4 by 4', function() {
-    assert.strictEqual(calculateNumber(4, 4, 'DIVIDE'), 1);
-  });
-  it('should return "Error" when type is DIVIDE and inputs are 1.4 and 0', function() {
-    assert.strictEqual(calculateNumber(1.4, 0, 'DIVIDE'), 'Error');
+
+  describe('type == "DIVIDE"', () => {
+    it('should return 1 when type is DIVIDE and inputs are 4 and 4', () => {
+      assert.strictEqual(calculateNumber(4, 4, 'DIVIDE'), 1);
+    });
+
+    it('should return 0.2 when type is DIVIDE and inputs are 1 and 5', () => {
+      assert.strictEqual(calculateNumber(1, 5, 'DIVIDE'), 0.2);
+    });
+
+    it('should return 0.2 when type is DIVIDE and inputs are 1.4 and 7', () => {
+      assert.strictEqual(calculateNumber(1.4, 7, 'DIVIDE'), 0.2);
+    });
+
+    it('should return "Error" when type is DIVIDE and inputs are 1.4 and 0', () => {
+      assert.strictEqual(calculateNumber(1.4, 0, 'DIVIDE'), 'Error');
+    });
   });
 });
